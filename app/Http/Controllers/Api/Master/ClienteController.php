a276057113d637f4e40a5a9214e7aa30838a5173
mateus4k <mateus4k@protonmail.ch>
2020-08-22 02:56:03 -0300

    refactor(controllers): change repository injection

--- app/Http/Controllers/Api/Master/ClienteController.php
+++ app/Http/Controllers/Api/Master/ClienteController.php
@@ -3,23 +3,22 @@
 namespace App\Http\Controllers\Api\Master;
 
 use App\Http\Controllers\Controller;
-use App\Http\Managers\ValorNfseManager;
-use App\Models\Cliente;
 use App\Repositories\ClienteRepository;
 
 class ClienteController extends Controller
 {
-    protected $clienteRepository;
+    private $repository;
 
-    public function __construct() {
-        $this->clienteRepository = new ClienteRepository();
+    public function __construct(ClienteRepository $repository)
+    {
+        $this->repository = $repository;
     }
 
     public function index()
     {
         try {
-            return response()->json($this->clienteRepository->all());
-        }catch (\Exception $exception){
+            return response()->json($this->repository->all());
+        } catch (\Exception $exception) {
             return response()->json($exception->getMessage(), 400);
         }
     }
