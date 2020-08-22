bc5209585a3c2b8584e1ef17822dd13d4570e37e
mateus4k <mateus4k@protonmail.ch>
2020-08-22 02:55:28 -0300

    feat(repositories): create base

---
+++ app/Repositories/BaseRepositoryInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseRepositoryInterface
+{
+    public function all(): Collection;
+
+    public function count(): int;
+
+    public function create(array $data): Model;
+
+    public function createMultiple(array $data): Collection;
+
+    public function delete();
+
+    public function deleteById($id): ?bool;
+
+    public function deleteMultipleById(array $ids): int;
+
+    public function first(): Model;
+
+    public function get(): Collection;
+
+    public function getById($id): Model;
+
+    public function limit(int $limit): BaseRepository;
+
+    public function orderBy(string $column, string $direction = 'asc'): BaseRepository;
+
+    public function updateById($id, array $data): Model;
+
+    public function where(string $column, string $value, string $operator = '='): BaseRepository;
+
+    public function whereIn(string $column, $values): BaseRepository;
+
+    public function with($relations): BaseRepository;
+}
