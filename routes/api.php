8412d749fbf2e01097afbff3d880ba3fad2a0470
mateus4k <mateus4k@protonmail.ch>
2020-08-23 04:08:02 -0300

    feat(tenancy): trying to set a global scoped for the tenant

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::namespace('Api')->group(function () {
     // Controllers Within The "App\Http\Controllers\Api" Namespace
     Route::post('/register'     , 'AuthController@register'         )->name('register');
     Route::post('/login'        , 'AuthController@login'            )->name('login');
-    Route::resource('conversations', 'Cliente\ConversationController', ['as' => 'conversations']);
-    Route::put('conversations/transfer/:id', 'Cliente\ConversationController@transfer');
+
+    Route::middleware(['cliente'])->group(function () {
+        Route::resource('conversations', 'Cliente\ConversationController', ['as' => 'conversations']);
+        Route::put('conversations/transfer/:id', 'Cliente\ConversationController@transfer');
+    });
 
 });
 
@@ -40,10 +32,12 @@ Route::middleware('auth:api')->group(function() {
 
         Route::namespace('Cliente')->group(function () {
             // Controllers Within The "App\Http\Controllers\Api\Master" Namespace
-            Route::resources([
-                'channels'      => 'ChannelController',
-                'departments'   => 'DepartmentController',
-            ]);
+            Route::middleware(['cliente'])->group(function () {
+                Route::resources([
+                    'channels'      => 'ChannelController',
+                    'departments'   => 'DepartmentController',
+                ]);
+            });
         });
 
     });
