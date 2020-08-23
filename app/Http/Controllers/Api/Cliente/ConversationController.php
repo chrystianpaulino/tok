af011162b3cc2bafd1d226c7a14eb7230a8cff28
Chrystian Bee <chrystianpaulino@beedelivery.com.br>
2020-08-23 02:55:59 -0300

    feat(conversation): upsert on controller

--- app/Http/Controllers/Api/Cliente/ConversationController.php
+++ app/Http/Controllers/Api/Cliente/ConversationController.php
@@ -23,14 +23,33 @@ class ConversationController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name'          => 'required',
-            'telefone'      => 'required',
-            'cpf'           => 'required',
-            'department_id' => 'required',
-            'channel_id'    => 'required',
+            'user_id'         => 'required',
+            'user_name'       => 'required',
+            'user_telefone'   => 'required',
+            'user_cpf'        => 'required',
+            'user_email'      => 'required',
+            'department_id'   => 'required',
+            'channel_id'      => 'required',
+            'conversation_id' => 'required',
+            'cliente_id'      => 'required',
         ]);
 
-        $result = $this->service->upsert($request->all());
+        $exists = [
+            'conversation_id' => $request->get('conversation_id'),
+            'user_cpf'        => $request->get('user_cpf'),
+            'user_email'      => $request->get('user_email'),
+            'channel_id'      => $request->get('channel_id'),
+            'cliente_id'      => $request->get('cliente_id'),
+            'department_id'   => $request->get('department_id'),
+        ];
+
+        $data = [
+            'user_telefone' => $request->get('user_telefone'),
+            'user_name'     => $request->get('user_name'),
+            'user_id'       => $request->get('user_id'),
+        ];
+
+        $result = $this->service->upsert($exists, $data);
         return response()->json($result);
     }
 
